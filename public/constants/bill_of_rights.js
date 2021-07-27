d6f33b655177970007a81eaebbc8520e248a1bce
Dan Cabaniss <dccabs@gmail.com>
2021-07-27 15:58:39 -0600

    add privacy policy

---
+++ public/constants/bill_of_rights.js
@@ -0,0 +1,162 @@
+const billOfRights = (
+  <p>
+      <h4>ACKNOWLEDGEMENT OF CONDITIONS FOR TREATMENT AND FINANCIAL DISCLOSURE</h4>
+          The undersigned patient and/or patient&#39;s representative here by
+          acknowledge receipt of HouseCall MD LLC&#39;s (HCMD LLC) document
+          entitled &quot;notice of privacy practices, conditions for treatment,
+          financial disclosure&#39;s, patient&#39;s right materials, financial
+          assistance, and &quot;referenced here as the handout. CONSENT FOR CARE:
+          I agreed to care and treatment by HCMD LLC and physicians, surgeons and
+          other licensed independent and dependent practitioners involved in my
+          care, together with other health care professionals employed by her
+          otherwise affiliated with HCMD LLC who are designated to provide care
+          for me. This consent may include examinations, tests, imaging studies,
+          labs, anesthesia, and medical or surgical treatment(s). Additional
+          documents and consent forms may be required for specific procedures. I
+          understand I have the right to ask questions about my care at any time,
+          and to be involved in my care decisions.
+          <br />
+          <br />
+          RISK OF TREATMENT: NO GUARANTEE OF RESULTS OR CURE: No promise or
+          guarantee of results or cure has been made to me. I know there are risks
+          related to surgical, medical, or diagnostic procedure(s). These risks
+          include the potential for infection, blood clots in veins and lungs,
+          bleeding, allergic reactions, and death.
+          <br />
+          <br />
+          PHOTOGRAPHS FOR TREATMENT, DIAGNOSIS AND/OR IDENTIFICATION: For
+          diagnosis and treatment purposes, I allow images such as photographs to
+          be taken and used. This includes video and electronic monitoring or
+          recording methods. These images may be used to add to written
+          information about my illness or injury. Some images are used once and
+          immediately discarded when no longer needed. Others may be kept as part
+          of my medical record, at the option of my treatment providers.
+          Photographs of me may also be taken for identification purposes.
+          <br />
+          <br />
+          IMAGES OR RECORDINGS OF HEALTHCARE PROVIDERS: I understand I must obtain
+          permission of all healthcare providers and any other individuals present
+          before I can take photographs or video of any members of my care team. I
+          also understand I cannot record conversations by any means without first
+          obtaining the permission of all persons being recorded.
+          <br />
+          <br />
+          NON-EMPLOYED PHYSICIANS AND PROVIDERS: I understand there are physicians
+          and other licensed providers who practice at HCMD LLC who are not
+          employed by HCMD LLC. These individuals are independent providers and
+          are not employed or agents of HCMD LLC. These include specialists such
+          as psychiatrists, dermatologists, and neurologists; among others. It
+          also includes nutritionist, dietitians, psychologists, and exercise
+          physiologists. I understand these providers use their own independent
+          judgment and there care and treatment plans. HCMD LLC does not control
+          the medical care and treatment given by these providers. I understand
+          that he may receive separate bills for services provided by those
+          parties.
+          <br />
+          <br />
+          FINANCIAL AGREEMENT: I agree to pay HCMD LLC for care at its regular
+          rates in terms applicable to my care in any applicable health insurance
+          coverage I have. I permit HCMD LLC to appeal any denial received from my
+          insurance company. If the third-party payer will not pay, I agreed to
+          pay for the services given, subject to any applicable contractual or
+          governmental regulations. If I am incapacitated by my medical condition
+          because my of my injuries and a third party cannot be reached, I
+          understand that HCMD LLC may file a medical services lien as permitted
+          under RCW 60.44.010. (this lien attaches only to a portion of the
+          proceeds of any settlement between me and the party that caused any
+          harm.) If my bill is sent to a lawyer or collection agency, I will pay
+          all reasonable attorney fees and costs, together with interest in any
+          amounts otherwise found to be owing. Information about estimated charges
+          for health services is available upon request. I understand I have the
+          right to request this information. AGENTS AND CONTRACTS: Whenever HCMD
+          LLC Is referenced above, it is my intent to include its employees,
+          officers, agents, attorneys, first and third party liability and claims
+          agents, third-party claims administrators and collection agencies, as
+          well as their agents or employees, to receive any information the HCMD
+          llc would otherwise be entitled to receive.
+          <br />
+          <br />
+          MEDICARE: If I am a Medicare participant, I understand that I need to
+          pay for services that are not covered by the Medicare program. This may
+          include, but is not limited to, cosmetic surgery, dental care, take home
+          and &quot;over-the-counter &quot;medications, private duty nurses,
+          services not medically needed, personal items, services covered by car
+          or liability insurance, or where a third-party is otherwise responsible
+          for any accident or injury leading to my need for care, as well as any
+          services not otherwise covered by Medicare. <br />
+          <br />
+          COINSURANCE: There may be a coinsurance for care giving related to my
+          Medicare or other insurance benefits. I know I will need to pay any
+          higher coinsurance for services provided by hospital-based clinic or
+          department. If the services were given in a non-hospital based setting,
+          my coinsurance would be lower. PHONE, EMAIL, TEXT MESSAGING
+          AUTHORIZATIONS: I grant permission and consent to HCMD LLC:
+          <br />
+          <br />
+          (1) to contact me by phone at any phone number associated with me,
+          including wireless (cell) numbers;
+          <br />
+          <br />
+          (2) to leave answering machine and voicemail messages for me, and
+          included any such messages information required by law (including debt
+          collection laws) and/or regarding amounts owed by me;
+          <br />
+          <br />
+          (3) to send me text messages or emails using any email or cellular
+          device addresses I provide and;
+          <br />
+          <br />
+          (4) to use pre-recorded/artificial voice messages and/or and automatic
+          dilating service (an &quot;autodialer&quot;) in connection with any
+          communications made to me or related to my scheduled services and my
+          care, unless I have exercised an &quot;opt out &quot;option associated
+          with such emails or text messages or have otherwise notified HCMD LLC in
+          writing to discontinue such communications using those pathways. (I
+          understand that opt out processes may take up to ten (10) business days
+          to go into effect.) I understand that I am not required to accept
+          messages in these formats as a condition of receiving services at HCMD
+          LLC.
+          <br />
+          <br />
+          EMAIL CONTAINING PROTECTED HEALTH INFORMATION: <br />
+          <br />I understand that exchanging email, text or other written
+          communications with my health care provider(s) or other members of my
+          care team can result in protected health information being disclosed to
+          unauthorized persons, and that HCMD LLC cannot control who views such
+          information when sent in on encrypted form. I understand the HCMD LLC
+          offers access to Kareo chart information to all patients, which provides
+          a fully encrypted and protected pathway for communicating with most of
+          its providers, although not all HCMD LLC providers utilize this service.
+          If I initiate or respond to communications using on encrypted pathways,
+          I assume the risk that my information may be compromised, and I
+          authorize HCMD LLC and its providers to communicate with me using that
+          process, unless or until I choose to opt out of such communications
+          pathways by notifying HCMD LLC in writing, allowing up to 10 business
+          days to implement any change in my communications pathways.
+          <br />
+          <br />
+          ADVANCED DIRECTIVE/LIVING WILL/POLST FORMS: I understand that I have the
+          right to carry out an advanced directive for healthcare (often
+          referenced as a &quot;Living Will&quot;). I understand I can get
+          information on the advanced directive policy online. I understand that
+          the POLST form (physicians orders for life- sustaining treatment) may
+          not always serve as a substitute for an advanced directive. If I have
+          completed a POLST or advanced directive form, I agreed to provide a copy
+          of such forms to HCMD LLC. I also understand that I can complete a
+          separate advance directive for mental health.
+          <br />
+          <br />
+          HEALTHCARE POWER OF ATTORNEY/MENTAL HEALTH POWER OF ATTORNEY: I
+          understand I can nominate another person or persons to make healthcare
+          decisions for me at any time that I am unable to do so. These can
+          include routine healthcare decisions (including life and death
+          decisions) as well as mental health decisions. If I complete these
+          forms, I will provide HCMD LLC with copies, or otherwise tell HCMD LLC
+          where they are located.
+          <br />
+          <br />
+          This consent will remain valid for 3 years from the date of
+          acknowledgement.
+  </p>
+)
+export default billOfRights;
---
+++ public/constants/privacyPolicy.js
@@ -0,0 +1,108 @@
+const privacyPolicy = (
+  <p>
+    <h4>NOTICE OF PRIVACY PRACTICES – HOUSECALL MD AFFILIATED COVERED ENTITY</h4>
+
+    <strong>THIS NOTICE DESCRIBES HOW MEDICAL INFORMATION ABOUT YOU MAY BE USED AND DISCLOSED AND HOW YOU CAN GET ACCESS TO THIS INFORMATION. PLEASE REVIEW IT CAREFULLY.</strong>
+    <br />
+    <br />
+    This Notice of Privacy Practices (the “Notice”) describes how HouseCall MD LLC and the members of its Affiliated Covered Entity (collectively “we” or “our”) may use and disclose your protected health information to carry out treatment, payment or business operations and for other purposes that are permitted or required by law. An Affiliated Covered Entity is a group of health care providers under common ownership or control that designates itself as a single entity for purposes of compliance with the Health Insurance Portability and Accountability Act (“HIPAA”). We have elected to voluntarily substantially comply with the standards set forth in HIPAA. The members of the HouseCall MD Affiliated Covered Entity will share protected health information with each other for the treatment, payment, and health care operations of the HouseCall MD Affiliated Covered Entity and as permitted by HIPAA and this Notice of Privacy Practices. For a complete list of the members of the HouseCall MD Affiliated Covered Entity, please contact the HouseCall MD Privacy Office.
+    <br />
+    <br />
+    <strong>USES AND DISCLOSURES OF PROTECTED HEALTH INFORMATION:</strong>
+    <br />
+    <br />
+    Your protected health information may be used and disclosed by our health care providers, our staff, and others outside of our office that are involved in your care and treatment for the purpose of providing health care services to you, to support our business operations, to obtain payment for your care, and any other use authorized or required by law.
+    <br />
+    <br />
+    <strong>TREATMENT:</strong>
+    <br />
+    <br />
+    We will use and disclose your protected health information to provide, coordinate, or manage your health care and any related services. This includes the coordination or management of your health care with a third party. For example, your protected health information may be provided to a health care provider to whom you have been referred to ensure the necessary information is accessible to diagnose or treat you.
+    <br />
+    <br />
+    <strong>PAYMENT:</strong>
+    <br />
+    <br />
+    Your protected health information may be used to bill or obtain payment for your health care services. This may include certain activities that your health insurance plan may undertake before it approves or pays for your services, such as: making a determination of eligibility or coverage for insurance benefits and reviewing services provided to you for medical necessity.
+    <br />
+    <br />
+    <strong>HEALTH CARE OPERATIONS:</strong>
+    <br />
+    <br />
+    We may use or disclose, as needed, your protected health information in order to support the business activities of this office. These activities include, but are not limited to, improving quality of care, providing information about treatment alternatives or other health-related benefits and services, development or maintaining and supporting computer systems, legal services, and conducting audits and compliance programs, including fraud, waste and abuse investigations.
+    <br />
+    <br />
+    <strong>USES AND DISCLOSURES THAT DO NOT REQUIRE YOUR AUTHORIZATION:</strong>
+    <br />
+    <br />
+    We may use or disclose your protected health information in the following situations without your authorization. These situations include the following uses and disclosures: as required by law; for public health purposes; for health care oversight purposes; for abuse or neglect reporting; pursuant to Food and Drug Administration requirements; in connection with legal proceedings; for law enforcement purposes; to coroners, funeral directors and organ donation agencies; for certain research purposes; for certain criminal activities; for certain military activity and national security purposes; for workers’ compensation reporting; relating to certain inmate reporting; and other required uses and disclosures. Under the law, we must make certain disclosures to you upon your request, and when required by the Secretary of the Department of Health and Human Services to investigate or determine our compliance with the requirements of the Health Insurance Portability and Accountability Act (HIPAA). State laws may further restrict these disclosures.
+    <br />
+    <br />
+    <strong>USES AND DISCLOSURES THAT REQUIRE YOUR AUTHORIZATION:</strong>
+    <br />
+    <br />
+    Other permitted and required uses and disclosures will be made only with your consent, authorization or opportunity to object unless permitted or required by law. Without your authorization, we are expressly prohibited from using or disclosing your protected health information for marketing purposes. We may not sell your protected health information without your authorization. Your protected health information will not be used for fundraising. If you provide us with an authorization for certain uses and disclosures of your information, you may revoke such authorization, at any time, in writing, except to the extent that we have taken an action in reliance on the use or disclosure indicated in the authorization.
+    <br />
+    <br />
+    <strong>YOUR RIGHTS WITH RESPECT TO YOUR PROTECTED HEALTH INFORMATION:</strong>
+    <br />
+    <br />
+    You may request access to or an amendment of your protected health information.
+    <br />
+    <br />
+    You have the right to request a restriction on the use or disclosure of your protected health/personal information. Your request must be in writing and state the specific restriction requested and to whom you want the restriction to apply. We are not required to agree to a restriction that you may request, except if the requested restriction is on a disclosure to a health plan for a payment or health care operations purpose regarding a service that has been paid in full out-of-pocket.
+    <br />
+    <br />
+    You have the right to request to receive confidential communications from us by alternative means or at an alternate location. We will comply with all reasonable requests submitted in writing which specify how or where you wish to receive these communications.
+    <br />
+    <br />
+    You have the right to request an amendment of your projected health information. If we deny your request for amendment, you have the right to file a statement of disagreement with us. We may prepare a rebuttal to our statement and we will provide you with a copy of any such rebuttal.
+    <br />
+    <br />
+    You have the right to receive an accounting of certain disclosures of your protected health information that we have made, paper or electronic, except for certain disclosures which were pursuant to an authorization, for purposes of treatment, payment, healthcare operations (unless the information is maintained in an electronic health record); or for certain other purposes.
+    <br />
+    <br />
+    You have the right to obtain a paper copy of this Notice, upon request, even if you have previously requested its receipt electronically by e-mail.
+    <br />
+    <br />
+    <strong>REVISIONS TO THIS NOTICE:</strong>
+    <br />
+    <br />
+    We reserve the right to revise this Notice and to make the revised Notice effective for protected health information we already have about you as well as any information we receive in the future. You are entitled to a copy of the Notice currently in effect. Any significant changes to this Notice will be posted on our web site. You then have the right to object or withdraw as provided in this Notice.
+    <br />
+    <br />
+    <strong>BREACH OF HEALTH INFORMATION:</strong>
+    <br />
+    <br />
+    We will notify you if a reportable breach of your unsecured protected health information is discovered. Notification will be made to you no later than 60 days from the breach discovery and will include a brief description of how the breach occurred, the protected health information involved and contact information for you to ask questions.
+    <br />
+    <br />
+    <strong>COMPLAINTS:</strong>
+    <br />
+    <br />
+    Complaints about this Notice or how we handle your protected health information should be directed to our HIPAA Privacy Officer. If you are not satisfied with the manner in which a complaint is handled you may submit a formal complaint to the Department of Health and Human Services, Office for Civil Rights by sending a letter to 200 Independence Avenue, S.W., Washington, D.C. 20201, calling 1-877-696-6775, or visiting www.hhs.gov/ocr/privacy/hipaa/complaints. We will not retaliate against you for filing a complaint.
+    <br />
+    <br />
+    We must follow the duties and privacy practices described in this Notice. We will maintain the privacy of your protected health information and to notify affected individuals following a breach of unsecured protected health information. If you have any questions about this Notice, please contact us at 1 (833) 432-5633 and ask to speak with our HIPAA Privacy Officer.
+    <br />
+    <br />
+    <strong>CONTACTING US</strong>
+    <br />
+    <br />
+    If you have any questions about this Privacy Policy, please contact us at:
+    <br />
+    <br />
+    How to Contact Us :
+    <br />
+    <br />
+    HouseCall MD LLC PO Box 3458
+    <br />
+    Silverdale, WA 98383
+    <br />
+    Telephone: +1(833)432-5633
+    <br />
+    Email: <a href="mailto:contact@housecallmd.org">contact@housecallmd.org</a>
+
+  </p>
+)
+export default privacyPolicy;
