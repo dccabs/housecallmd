93448b8b3468b0da85b92bf2e4692160417d500d
Dan Cabaniss <dccabs@gmail.com>
2022-02-20 11:11:48 -0600

    new endpoints

--- pages/api/getPatientsByFacilityId.js
+++ pages/api/getPatientsByFacilityId.js
@@ -1,1 +1,20 @@
+import { supabase } from '../../utils/initSupabase'
 
+const getFacilityPatientById = async (req, res) => {
+  const token = req.headers.token
+  const { id } = req.body
+
+  if (!id || id === 'undefined') {
+    throw Error('User ID not found')
+  }
+
+  let { data: patients, error } = await supabase
+    .from('facility_patients')
+    .select('*')
+    .eq('id', id)
+
+  if (error) return res.status(401).json({ error: error.message })
+  return res.status(200).json(patients[0])
+}
+
+export default getFacilityPatientById
